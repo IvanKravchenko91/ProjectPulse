78c5b1f9ae1c7f14ff68937c91250fbdce66d0a4
Ivan <vanya.kravchenko9121@gmail.com>
2022-12-09 09:03:37 +0100

    #sections advantage

---
+++ dist/src/js/script.js
@@ -0,0 +1,21 @@
+//number
+//string - "", ''.
+//true/false
+//null
+//underfined
+
+// let obj = {
+//    name: 'apple'
+//    color: 'green'
+//    width: '200'
+//}
+//symbol
+
+//alert(123345)
+//console.log("sfdds")
+//let answer = confirm('Вам есть 18?');
+//console.log(answer)
+
+//let answer = prompt('Вам есть 18?', "");
+//console.log(answer)
+
