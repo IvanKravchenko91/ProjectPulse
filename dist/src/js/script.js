c93ae97d85e1549404a67c5d0ca7c14838ae4c31
Ivan <vanya.kravchenko9121@gmail.com>
2022-12-09 22:06:10 +0100

    #section carousel

--- dist/src/js/script.js
+++ dist/src/js/script.js
@@ -1,21 +1,17 @@
-//number
-//string - "", ''.
-//true/false
-//null
-//underfined
-
-// let obj = {
-//    name: 'apple'
-//    color: 'green'
-//    width: '200'
-//}
-//symbol
-
-//alert(123345)
-//console.log("sfdds")
-//let answer = confirm('Вам есть 18?');
-//console.log(answer)
-
-//let answer = prompt('Вам есть 18?', "");
-//console.log(answer)
-
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                dots: true,
+                arrow: false,
+                }
+            }
+        ]
+      });
+  });
