d8091dd57e4c5bbabf6d033e6151f98122d701f9
Ivan <vanya.kravchenko9121@gmail.com>
2022-12-12 13:17:14 +0100

    #section catalog finish

--- dist/src/js/script.js
+++ dist/src/js/script.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
-    $('.carousel_inner').slick({
+    $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: false,
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 992,
                 settings: {
-                dots: true,
-                arrow: false,
+                    dots: true,
+                    arrows: false
                 }
             }
         ]
-      });
+    });
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
   });
